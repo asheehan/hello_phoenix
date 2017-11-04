78dbeedab8849906d9fc3d38ccc45308595c5167
Andrew Sheehan <sheehan.andrew@gmail.com>
2017-11-04 15:32:29 -0500

    Add distillery library

--- mix.exs
+++ mix.exs
@@ -40,7 +40,10 @@ defmodule HelloPhoenix.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+
+      # deployment
+      {:distillery, "~> 1.5", runtime: false},
     ]
   end
 
