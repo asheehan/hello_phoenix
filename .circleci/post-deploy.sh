dced0edadbcc6ca9e2d75d0d22a3b34b1f012aeb
Andrew Sheehan <sheehan.andrew@gmail.com>
2017-11-04 15:15:21 -0500

    Add circle ci integration

---
+++ .circleci/post-deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ssh $APP_USER@$APP_HOST <<ENDSSH
+  echo "changing dir to ./apps/$APP_NAME"
+  cd ./apps/$APP_NAME;
+  date >> deploy-log.txt;
+
+  echo "Symlinking static"
+  ln -sfn lib/$APP_NAME-$APP_VERSION/priv/static static;
+
+  echo "stopping app";
+  ./bin/$APP_NAME stop >> deploy-log.txt || true;
+ENDSSH
+
+ssh $APP_USER@$APP_HOST <<ENDSSH
+  cd ./apps/$APP_NAME;
+  date >> deploy-log.txt;
+
+  # You may want to use this if you are using Cowboy without proxy
+  # echo "giving beam permission to start the webserver"
+  # sudo setcap 'cap_net_bind_service=+ep' /home/$APP_USER/$APP_NAME/erts-9.1.2/bin/beam.smp;
+
+  echo "starting app";
+  PORT=4000 ./bin/$APP_NAME start >> deploy-log.txt;
+
+  echo "Finished";
+ENDSSH
